c7a8de2eadf745a1fb7e305c9b25d328f4451278
wdeveloperr <119066150+wdeveloperr@users.noreply.github.com>
2022-11-25 22:21:12 +0530

    nb

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/wdeveloperr/wsFinderBOT.git /wsFinderBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /wsFinderBOT
 fi
-cd /LazyPrincess
+cd /wsFinderBOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
